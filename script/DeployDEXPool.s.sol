1106b53bd01414daaa40ce1c87841690b6167a4a
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2025-04-09 11:48:14 +0530

    Update DeployDEXPool.s.sol

--- script/DeployDEXPool.s.sol
+++ script/DeployDEXPool.s.sol
@@ -8,12 +8,13 @@ contract DeployDEXPool is Script {
     address constant FACTORY = 0x589a43d979dA679E9D8fc0fa72C31db81Cf91afC;
     address constant WC2FLR = 0xC67DCE33D7A8efA5FfEB961899C73fe01bCe9273;
     address constant testUSD = 0x6623C0BB56aDb150dC9C6BdB8682521354c2BF73;
+    address ftsoV2Address = 0xBF55AFB5d543db03f55852B02973B24345EAd1e1;
 
     function run() external {
         vm.startBroadcast();
 
         DEXFactory factory = DEXFactory(FACTORY);
-        address pool = factory.createPool(WC2FLR, testUSD);
+        address pool = factory.createPool(WC2FLR, testUSD, ftsoV2Address);
 
         console.log("DEX Pool deployed at:", pool);
 
