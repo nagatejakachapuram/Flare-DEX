15cfd09ed8e69c998203f583deae2fbfb6688772
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2025-04-15 11:38:11 +0530

    Update FTSOOracle.sol

--- src/dex/FTSOOracle.sol
+++ src/dex/FTSOOracle.sol
@@ -2,32 +2,31 @@
 pragma solidity 0.8.19;
 
 import {TestFtsoV2Interface} from "../../lib/flare-periphery-contracts/flare/TestFtsoV2Interface.sol";
+
 import {ContractRegistry} from "../../lib/flare-periphery-contracts/coston2/ContractRegistry.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 interface IFTSOOracle {
     function fetchPrice(
-        uint256 feedIndex
+        uint8 feedArrayIndex
     ) external view returns (uint256, int8);
 }
-contract MockFTSOOracle is IFTSOOracle {
-    uint256 public price;
-    int8 public decimals;
 
-    constructor(uint256 _price, int8 _decimals) {
-        price = _price;
-        decimals = _decimals;
+contract FTSOOracle {
+    bytes21[] private feedIds;
+    TestFtsoV2Interface internal ftsoV2;
+
+    // Constructor to initialize the contract with feed IDs
+    constructor(bytes21[] memory _feedIds) {
+        feedIds = _feedIds;
+        ftsoV2 = TestFtsoV2Interface(address(ContractRegistry.getTestFtsoV2()));
     }
 
-    function fetchPrice(uint256) external view returns (uint256, int8) {
-        return (price, decimals);
-    }
-
-    function setPrice(uint256 _price) external {
-        price = _price;
-    }
-
-    function setDecimals(int8 _decimals) external {
-        decimals = _decimals;
+    // Function to fetch the price from the FTSO
+    function fetchPrice(
+        uint8 _feedArrayIndex
+    ) public view returns (uint256, int8) {
+        bytes21 feedId = feedIds[_feedArrayIndex];
+        (uint256 _value, int8 _decimals, ) = ftsoV2.getFeedById(feedId);
+        return (_value, _decimals);
     }
 }
