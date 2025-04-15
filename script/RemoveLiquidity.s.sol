a46d19daa236ff6cc12ff1e7c03c0597f5fdd328
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2025-04-15 11:41:04 +0530

    Update RemoveLiquidity.s.sol

--- script/RemoveLiquidity.s.sol
+++ script/RemoveLiquidity.s.sol
@@ -6,8 +6,8 @@ import "../src/dex/DEXPool.sol";
 
 contract RemoveLiquidity is Script {
     address constant POOL = 0x8c847Ad2bAfA13a50fc319A6d7389405666dbe72;
-    int24 public lowerTick;
-    int24 public upperTick;
+    int24 public lowerTick = -600;
+    int24 public upperTick = 600;
 
     uint256 constant LIQUIDITY = 1e18;
 
@@ -15,6 +15,8 @@ contract RemoveLiquidity is Script {
         vm.startBroadcast();
 
         DEXPool(POOL).removeLiquidity(lowerTick, upperTick, uint128(LIQUIDITY));
+
+        console.log("Liquidity removed from the pool.");
 
         vm.stopBroadcast();
     }
